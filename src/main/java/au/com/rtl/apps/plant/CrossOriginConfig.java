cb9abf5a2f0c0dd99f2d9c4e95bd1e78f70b61c8
Vikash Sharma <vikashsharma089@gmail.com>
2022-07-01 17:04:07 +0530

    CrossOrigin Fix
    Signed-off-by: Vikash Sharma <vikashsharma089@gmail.com>

---
+++ src/main/java/au/com/rtl/apps/plant/CrossOriginConfig.java
@@ -0,0 +1,21 @@
+package au.com.rtl.apps.plant;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CrossOriginConfig implements WebMvcConfigurer {
+	
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/operations/jobs/plant/prestart/**").allowedOrigins("*");
+			}
+		};
+	}
+
+}
